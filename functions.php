3169a877ca2022ef8d5acc7be19e29f116e4f925
BeTr4y4l <75473312+BeTr4y4l@users.noreply.github.com>
2021-07-01 22:22:39 +0200

    v1.1 after online crash

--- functions.php
+++ functions.php
@@ -21,18 +21,13 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page('Administration');	
 }
 
-
-
-// A SUPPRIMER AVANT MISE EN PROD -> SUPPRIME UNE ERREUR LIEE AU DEBUGGER
-
 /**
  * Proper ob_end_flush() for all levels
  *
  * This replaces the WordPress `wp_ob_end_flush_all()` function
  * with a replacement that doesn't cause PHP notices.
- */
+  */
 remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
 add_action( 'shutdown', function() {
    while ( @ob_end_flush() );
 } );
-
